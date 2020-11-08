ab9b2db08808145fb7037ad820cc8866a2337780
Damir Shaikhattarov <damirshaikhattarov@gmail.com>
2020-11-08 23:07:11 +0300

    ОМС 08.11.2020

---
+++ ОМС Основы моделирования систем/Lab_3/VS_project/Coin/Coin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Coin {
+    class Coin {
+        readonly byte[] sequence = new byte[1000];
+        readonly byte key = 1;
+        byte Flip() => (byte) new Random().Next(0,2);
+        public Coin() { }
+        public void Test() {
+            Console.WriteLine("Программная имитация броска монетки:");
+            InitSequence();
+            Console.WriteLine("Вычисление вероятности выпадания решки:");
+            FindKey();
+        }
+        void InitSequence() {
+            for(int i = 0; i < 1000; i++) { 
+                sequence[i] = Flip();
+                if (i < 100) {
+                    if (sequence[i] == 0)
+                        Console.Write("0");
+                    else
+                        Console.Write("Р");
+                }
+                
+            }
+            Console.WriteLine("\n");
+        }
+        void FindKey() {
+            for (int N = 100; N <= 1000; N += 100) {
+                int count = 0;
+                for (int i = 0; i < N; i++) {
+                    if (sequence[i] == key) { count++; }
+                }
+                Console.WriteLine("Количество бросков: {0}; Присутствует данная комбинация: {1} раз;\n", N, count);
+            }
+        }
+    }
+}
---
+++ ОМС Основы моделирования систем/Lab_3/VS_project/Coin/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace Coin {
+    class Program {
+        static void Main() {
+            new Coin().Test();
+        }
+    }
+}
